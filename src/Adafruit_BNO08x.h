a44558bcc2641d56c2de4c28aa79fe563df39e38
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2025-07-06 22:43:26 +0900

    update for syulime

--- src/Adafruit_BNO08x.h
+++ src/Adafruit_BNO08x.h
@@ -14,33 +14,33 @@
  *	BSD license (see license.txt)
  */
 
-#ifndef _ADAFRUIT_BNO08x_H
-#define _ADAFRUIT_BNO08x_H
+#ifndef _SYULIMETRACKER_BNO08x_H
+#define _SYULIMETRACKER_BNO08x_H
 
 #include "Arduino.h"
 #include "sh2.h"
 #include "sh2_SensorValue.h"
-#include "sh2_err.h"
-#include <Adafruit_BusIO_Register.h>
-#include <Adafruit_I2CDevice.h>
-#include <Adafruit_Sensor.h>
+#include "sh2_err.h" #include "../../SyulimeTracker_BusIO/SyulimeTracker_BusIO_Register.h"
+#include "../../SyulimeTracker_BusIO/SyulimeTracker_I2CDevice.h"
+#include "Adafruit_Sensor.h"
 #include <Wire.h>
 
 #define BNO08x_I2CADDR_DEFAULT 0x4A ///< The default I2C address
 
 /* Additional Activities not listed in SH-2 lib */
 #define PAC_ON_STAIRS 8 ///< Activity code for being on stairs
-#define PAC_OPTION_COUNT                                                       \
+#define PAC_OPTION_COUNT \
   9 ///< The number of current options for the activity classifier
 
 /*!
  *    @brief  Class that stores state and functions for interacting with
  *            the BNO08x 9-DOF Orientation IMU Fusion Breakout
  */
-class Adafruit_BNO08x {
+class SyulimeTracker_BNO08x
+{
 public:
-  Adafruit_BNO08x(int8_t reset_pin = -1);
-  ~Adafruit_BNO08x();
+  SyulimeTracker_BNO08x(int sda = -1, int scl = -1, int8_t reset_pin = -1);
+  ~SyulimeTracker_BNO08x();
 
   bool begin_I2C(uint8_t i2c_addr = BNO08x_I2CADDR_DEFAULT,
                  TwoWire *wire = &Wire, int32_t sensor_id = 0);
@@ -62,6 +62,11 @@ protected:
 
   sh2_Hal_t
       _HAL; ///< The struct representing the SH2 Hardware Abstraction Layer
+
+private:
+  int _sda;          ///< SDA pin number for I2C
+  int _scl;          ///< SCL pin number for I2C
+  int8_t _reset_pin; ///< Reset pin number
 };
 
 #endif
